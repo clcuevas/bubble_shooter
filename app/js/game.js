881d88d0509f47981bdfacf8e7ff146b042a3b33
Claudia Cuevas <omordn@Claudias-iMac.local>
2015-09-18 21:33:02 -0700

    add bubble class and object to display in DOM

--- app/js/game.js
+++ app/js/game.js
@@ -3,13 +3,23 @@
 var BubbleShoot = window.BubbleShoot || {};
 BubbleShoot.Game = (function($) {
   var Game = function() {
-    var startGame = function() {
-      $('.but_start_game').unbind('click');
-      BubbleShoot.ui.hideDialog();
-    };
+    var curBubble;
 
     this.init = function() {
       $('.but_start_game').bind('click', startGame);
+    };
+
+    var startGame = function() {
+      $('.but_start_game').unbind('click');
+      BubbleShoot.ui.hideDialog();
+      curBubble = getNextBubble();
+    };
+
+    var getNextBubble = function() {
+      var bubble = BubbleShoot.Bubble.create();
+      bubble.getSprite().addClass('cur_bubble');
+      $('#board').append(bubble.getSprite());
+      return bubble;
     };
   };
   return Game;
