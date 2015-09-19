e861d7fdf23cb109769f7742271e28c59c2dbada
Claudia Cuevas <omordn@Claudias-iMac.local>
2015-09-18 20:55:32 -0700

    add server file, and app files. working jQuery fadeout feature

---
+++ app/js/ui.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var BubbleShoot = window.BubbleShoot || {};
+BubbleShoot.ui = (function($) {
+  var ui = {
+    init: function() {
+
+    },
+    hideDialog: function() {
+      $('.dialog').fadeOut(300);
+    }
+  };
+  return ui;
+})(jQuery);
