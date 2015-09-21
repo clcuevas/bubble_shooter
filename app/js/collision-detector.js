7fa9bd8a24468b974c93a5ab7f533b61764d01cf
Claudia Cuevas <omordn@Claudias-iMac.local>
2015-09-20 21:31:42 -0700

    add comments in collision script for math portion.. TOO MUCH MATH!

--- app/js/collision-detector.js
+++ app/js/collision-detector.js
@@ -1,16 +1,27 @@
 'use strict';
-
+/*
+  The math portion that is coded inside the findIntersection method
+  is trigonometry.
+*/
 var BubbleShoot = window.BubbleShoot || {};
+//create an object called CollisionDetector
 BubbleShoot.CollisionDetector = (function($) {
   var CollisionDetector = {
+    //curBubble is an instance of the Bubble class
+    //board is an instance of the object Board
+    //angle at which the bubble is being fired
+    //these three arguments are needed to know about the starting situation
     findIntersection: function(curBubble, board, angle) {
       var rows = board.getRows();
+      //collision holds the bubble that's been collided with
       var collision = null;
+      //pos and start retrieves the bubble's starting position as an obj
       var pos = curBubble.getSprite().position();
       var start = {
         left: pos.left + BubbleShoot.ui.BUBBLE_DIMS / 2,
         top: pos.top + BubbleShoot.ui.BUBBLE_DIMS / 2
       };
+      //dx, dy define how much a bubble moves left or right (dx) or up (dy)
       var dx = Math.sin(angle);
       var dy = -Math.cos(angle);
 
@@ -20,6 +31,7 @@ BubbleShoot.CollisionDetector = (function($) {
         for (var j = 0; j < row.length; j++) {
           var bubble = row[j];
 
+          //start calculating collisions, geometry being used
           if (bubble) {
             var coords = bubble.getCoords();
 
@@ -28,9 +40,12 @@ BubbleShoot.CollisionDetector = (function($) {
               y: start.top - coords.top
             };
 
+            //this line tells us at what proportion of the fired bubble's total movement it will be closest to the candidate bubble's center
             var t = dx * distToBubble.x + dy * distToBubble.y;
+            //calculate where the collision happens
             var ex = -t * dx + start.left;
             var ey = -t * dy + start.top;
+            //if the distance is less than double the candiate bubble's radius, a collision occurs
             var distEC = Math.sqrt((ex - coords.left) * (ex - coords.left) + (ey - coords.top) * (ey - coords.top));
 
             if (distEC < BubbleShoot.ui.BUBBLE_DIMS * 0.75) {
@@ -76,6 +91,8 @@ BubbleShoot.CollisionDetector = (function($) {
           } //end if (bubble) condition
         } //end inner for loop
       } //end outer for loop
+
+      //return the collision object that stores all the collision data
       return collision;
     }
   };
